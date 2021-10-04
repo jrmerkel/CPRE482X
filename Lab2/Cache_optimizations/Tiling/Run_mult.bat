d3df688cda4d5336b6da5418bb4f0e0ee593a682
root <root@cpre482-05.ece.iastate.edu>
2021-10-04 13:55:52 -0500

    add remaining tensorboard part

---
+++ Lab2/Cache_optimizations/Tiling/Run_mult.bat
@@ -0,0 +1,3 @@
+FOR /L %%i IN (1,1,100) DO (
+    C:\Temp\CPRE482X\Lab2\cpre482x-lab2\Cache_optimized\Aligned_array_access\TinyImagenet_Inference_Inference_clean\Release\TinyImagenet_Inference_Inference_clean.exe
+)
