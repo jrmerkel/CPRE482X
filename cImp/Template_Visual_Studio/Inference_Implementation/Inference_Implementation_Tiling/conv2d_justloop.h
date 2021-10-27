14d4d5812686ce9adaf3de9e89e4cfe63b3c8f24
jrmerkel <jrmerkel@cpre482-13.ece.iastate.edu>
2021-10-26 20:25:22 -0500

    idk where jupyter is

--- cImp/Template_Visual_Studio/Inference_Implementation/Inference_Implementation_Tiling/conv2d_justloop.h
+++ cImp/Template_Visual_Studio/Inference_Implementation/Inference_Implementation_Tiling/conv2d_justloop.h
@@ -21,34 +21,15 @@ void conv2d(float (&input_tensor)[w][h][c], float (&weights)[s][r][c][m], float 
 	{
 		for(int y = 0; y < p; y++)////////////////////////////p = y
 		{
-				for(int k = 0; k < s; k++)////////////////////c = i
-
+			for(int x = 0; x < m; x++)////////////////////////q = z
 			{
+				for(int k = 0; k < s; k++)////////////////////c = i
+				{
 
 					for(int j = 0; j < r; j++)////////////////r = j
-				{
-					for(int i = 0; i < c; i++)////////////s = k
 					{
-				
-				// for(int ii = i; ii < min(c,i+BLOCK_SIZE); ii++)
-				// {
-				// for(int xx = x; xx < min(m, x+BLOCK_SIZE); xx++)
-					
-				// 		{
-				// 	for(int zz =z; zz<min(q, z+BLOCK_SIZE); zz++)
-
-				// 		{
-				// 	for(int yy = y; yy < min(p, y+BLOCK_SIZE); yy++)
-
-							//{
-					for(int x = 0; x < m; x++)////////////////////////q = z
-								{
-																		
-								// for(int kk = k; kk < min(k+BLOCK_SIZE, s); kk++)
-								// {
-								// 	for(int jj = j; jj< min(j+BLOCK_SIZE,r); jj++)
-								// 	{
-
+					for(int i = 0; i < c; i++)////////////s = k
+						{
 											// 	//matrix[m][p][q] = i[c][p + r][q + s] * f[m][c][r][s]
 												//Ours is not in this exact order
 												//Ours is matrix[m][p][q] = i[q + s][p+r][c] * f[s][r][c][m]
